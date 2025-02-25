e5d4d9337a96dff1b1f0c9c6b3489c1c79b251b9
duanxiaojie <duanlangjd@126.com>
2025-02-25 15:23:08 +0800

    update

--- intersms/xsend.go
+++ intersms/xsend.go
@@ -1,59 +1,107 @@
 package intersms
 
 import (
+	"bytes"
 	"crypto/sha256"
 	"encoding/hex"
+	"encoding/json"
+	"fmt"
+	"io"
+	"net/http"
 	"sort"
 	"strings"
+	"time"
 )
 
 const (
-	XSEND_API = "https://api-hk-v4.mysubmail.com/internationalsms/xsend"
+	XSEND_API    = "https://hk-api-v4.mysubmail.com/internationalsms/xsend"
+	SIGN_VERSION = "2"
+	SIGN_TYPE    = "sha256"
+	CONTENT_TYPE = "application/json;charset=utf-8"
 )
 
 type Xsend struct {
-	appid  string
-	appkey string
-
-	signature string
-	to        string
-	project   string
-	vars      []struct {
-		key string
-		val string
-	}
-	tag       string
-	timestamp string
-	signType  string
+	Appid       string `json:"appid"`
+	appkey      string
+	Signature   string            `json:"signature"`
+	To          string            `json:"to"`
+	Project     string            `json:"project"`
+	Vars        map[string]string `json:"vars"`
+	Tag         string            `json:"tag"`
+	Timestamp   string            `json:"timestamp"`
+	SignType    string            `json:"sign_type"`
+	SignVersion string            `json:"sign_version"`
 }
 
 // init with appid and appkey
-func Init(appid, appkey string) *Xsend {
+func NewXsend(appid, appkey string) *Xsend {
 	return &Xsend{
-		appid:  appid,
-		appkey: appkey,
+		Appid:       appid,
+		appkey:      appkey,
+		Vars:        make(map[string]string),
+		Timestamp:   fmt.Sprint(time.Now().UTC().Unix()),
+		SignType:    SIGN_TYPE,
+		SignVersion: SIGN_VERSION,
 	}
 }
 
 // set to address
 func (Xsend *Xsend) SetAddress(to string) {
-	Xsend.to = to
+	Xsend.To = to
 }
 
 // set project id
 func (Xsend *Xsend) SetProject(project string) {
-	Xsend.project = project
+	Xsend.Project = project
 }
 
-func (Xsend *Xsend) Send() string {
+// set vars
+func (Xsend *Xsend) AddVar(key, val string) {
+	Xsend.Vars[key] = val
+}
+
+// set tag
+func (Xsend *Xsend) SetTag(tag string) {
+	Xsend.Tag = tag
+}
+
+func (Xsend *Xsend) Send() (string, error) {
+	Xsend.Signature = BuildSignature(Xsend)
 	return Send(XSEND_API, Xsend)
 }
 
-func Send(XSEND_API string, Xsend *Xsend) string {
-	return "test"
+func Send(XSEND_API string, Xsend *Xsend) (string, error) {
+	data, _ := json.Marshal(Xsend)
+	body := bytes.NewBuffer([]byte(data))
+
+	client := &http.Client{
+		Timeout: 10 * time.Second,
+	}
+
+	retstr, err := client.Post(XSEND_API, CONTENT_TYPE, body)
+
+	if err != nil {
+		return "", err
+	}
+	defer retstr.Body.Close()
+
+	result, err := io.ReadAll(retstr.Body)
+	if err != nil {
+		return "", err
+	}
+
+	return string(result), nil
 }
 
-func BuildSignature(req map[string]string, appid, appkey, signtype string) string {
+func BuildSignature(data *Xsend) string {
+	req := map[string]string{
+		"to":           data.To,
+		"project":      data.Project,
+		"sign_version": data.SignVersion,
+		"appid":        data.Appid,
+		"timestamp":    data.Timestamp,
+		"sign_type":    data.SignType,
+	}
 	var arg []string
 	var keys []string
 	for k := range req {
@@ -64,7 +112,7 @@ func BuildSignature(req map[string]string, appid, appkey, signtype string) strin
 		arg = append(arg, key+"="+req[key])
 	}
 	argstr := strings.Join(arg, "&")
-	argstr = appid + appkey + argstr + appid + appkey
+	argstr = data.Appid + data.appkey + argstr + data.Appid + data.appkey
 	return SHA256(argstr)
 }
 
--- intersms/xsend_test.go
+++ intersms/xsend_test.go
@@ -1,13 +1,24 @@
-package intersms_test
+package intersms
 
 import (
 	"testing"
+)
 
-	"github.com/duanxiaojie/submail_sdk.git/intersms"
+const (
+	APPID  = "60005"
+	APPKEY = "440f691b53bf882390e78455fb8020ec"
 )
 
 func TestXsend(t *testing.T) {
-	x := &intersms.Xsend{}
-	r := x.Send()
-	t.Logf("result : %s", r)
+
+	x := NewXsend(APPID, APPKEY)
+	x.SetAddress("+8615201893074")
+	x.SetProject("8sgWm")
+	d, e := x.Send()
+
+	if e != nil {
+		t.Errorf("error : %s", e.Error())
+	} else {
+		t.Logf("result : %s", d)
+	}
 }
